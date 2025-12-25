7342b935f4ef22c204a7decc54f7d93ce8a67410
Sudeep <sudeepsudee828@gmail.com>
2025-12-25 19:37:37 +0530

    url updated

--- frontend/src/UserPage/Footer.jsx
+++ frontend/src/UserPage/Footer.jsx
@@ -3,7 +3,7 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import YouTubeIcon from "@mui/icons-material/YouTube";
-import logo from "../assets/zoom-logo-white.svg";
+import logo from "../../assets/zoom-logo-white.svg";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
