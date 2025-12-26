817fed8e8ba0c459f490e17dfcc962ebd9f8f0e6
Sudeep <sudeepsudee828@gmail.com>
2025-12-26 12:40:01 +0530

    support

--- frontend/src/UserPage/Support/Support.jsx
+++ frontend/src/UserPage/Support/Support.jsx
@@ -4,7 +4,9 @@ export default function Support(){
     return(
         <>
         <div className="support-main md:w-screen md:h-[600px] bg-gradient-to-br from-[#0f172a] via-[#1e1b4b] to-[#312e81]">
-                  
+               <div className="support-card md:w-xl h-[400px] bg-gray-100">
+                    
+                </div>   
         </div>
         </>
     )
