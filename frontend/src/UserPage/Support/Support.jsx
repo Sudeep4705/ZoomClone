36c1e12407fd613d79f1d5595270055d6eb33625
Sudeep <sudeepsudee828@gmail.com>
2025-12-26 13:55:32 +0530

    support

--- frontend/src/UserPage/Support/Support.jsx
+++ frontend/src/UserPage/Support/Support.jsx
@@ -3,7 +3,7 @@
 export default function Support(){
     return(
         <>
-        <div className="support-main md:w-screen md:h-[600px] bg-gradient-to-br from-[#0f172a] via-[#1e1b4b] to-[#312e81] md:flex md:justify-center">
+        <div className="support-main md:w-screen md:h-[600px] bg-gradient-to-br from-[#0f172a] via-[#1e1b4b] to-[#312e81] md:flex md:justify-center md:items-center">
                <div className="support-card md:w-xl h-[400px] bg-gray-100">
                     
                 </div>   
